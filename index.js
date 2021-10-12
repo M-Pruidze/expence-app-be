e87f0fccd7d95faf22ced793b420965470436550
M-Pruidze <m.pruidze.exceedteam@gmail.com>
2021-10-12 19:09:53 +0400

    connected to local mongodb

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const http = require('http');
 
 const port = process.env.PORT || 3000;
 
+
 const server = http.createServer(app);
 
 server.listen(port, () => console.log('server is listening on port', port));
